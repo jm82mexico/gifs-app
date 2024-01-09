c1f25baa7bc0a4fde26b274daedd86f3cf35ecbb
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-01-08 20:26:55 -0600

    crear peticion http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
 
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,6 +15,7 @@ import { GifsModule } from './gifs/gifs.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,27 +1,54 @@
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = [];
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'aXLqVw9yUCQdK9G4uBQr5Xhxsg4RpY8d';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+
+
+  constructor( private http: HttpClient) { }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void {
-    tag = tag.trim().toLowerCase();
+  private organizeHistory(tag:string){
+    tag = tag.toLowerCase();
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
 
     this._tagsHistory.unshift(tag);
-    // if (!this._tagsHistory.includes(tag)) {
-    //   this._tagsHistory.unshift(tag);
-    //   this._tagsHistory = this._tagsHistory.slice(0, 10);
-    // }
-    console.log(this._tagsHistory);
+    this._tagsHistory = this._tagsHistory.slice(0, 10);
+
+  }
+
+  searchTag(tag: string): void {
+    if (tag.trim().length === 0) {
+      return;
+    }
+
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag);
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+    .subscribe((response) => {
+      this.gifList = response.data;
+    });
+    
   }
 
 }

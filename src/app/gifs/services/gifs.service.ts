8eb446ad8857ccc964b2f8952fbe9188c33da7da
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-01-11 19:56:06 -0600

    local storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,7 +15,10 @@ export class GifsService {
 
 
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+  }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -29,7 +32,21 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.slice(0, 10);
+    this.saveLocalStorage();
 
+  }
+
+  private saveLocalStorage():void {
+    localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+  }
+
+  private loadLocalStorage():void {
+    if( !localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if ( this._tagsHistory.length === 0 ) return;
+    this.searchTag( this._tagsHistory[0] );
   }
 
   searchTag(tag: string): void {
